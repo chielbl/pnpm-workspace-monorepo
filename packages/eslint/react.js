3cf17332e5ce01a46303dfaa4b54f2a404ab765a
chielbl <chielbleyenbergh@gmail.com>
2024-03-28 16:42:01 +0100

    feat(eslint): add react linter

---
+++ packages/eslint/react.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/react',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
