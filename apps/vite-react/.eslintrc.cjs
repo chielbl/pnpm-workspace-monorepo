ebd9c576ec2ed8672fd125d66754af6490eba8da
chielbl <chielbleyenbergh@gmail.com>
2024-03-29 17:21:57 +0100

    feat(config folder): move eslint and tsconfig in one config folder

--- apps/vite-react/.eslintrc.cjs
+++ apps/vite-react/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  extends:['@repo/base.js','@repo/typescript.js' ,'@repo/browser.js', '@repo/react.js'],
+  extends:['@repo/config/eslint/base.js','@repo/tconfig/eslin/typescript.js' ,'@repo/tconfig/eslin/browser.js', '@repo/tconfig/eslin/react.js'],
   root: true,
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
