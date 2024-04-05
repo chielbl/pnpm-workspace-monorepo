065155f7c234339b9402cead79a0561985b48b2a
chielbl <chielbleyenbergh@gmail.com>
2024-04-05 10:44:34 +0200

    feat(use options controllers): create contollers/handler to use in option

--- apps/fastify-node/src/routes/items.ts
+++ apps/fastify-node/src/routes/items.ts
@@ -2,25 +2,29 @@ import { DoneFuncWithErrOrRes, FastifyInstance } from "fastify";
 import { ItemParams, getItemSchema, getItemsSchema } from "./schemas";
 import { DMY_API } from "../server";
 
+const getItemsHandler = async (_, reply) => {
+  const res = await fetch(DMY_API);
+  const items = await res.json();
+
+  reply.send({ items });
+};
+
+const getItemHandler = async (req: { params: ItemParams }, reply: any) => {
+  const { id } = req.params as ItemParams; // QUESTION: Why do we need to cast req.params as ItemParams and doesn't get to right type from getItemSchema?
+  const res = await fetch(`${DMY_API}${id}`);
+  const item = await res.json();
+
+  reply.send({ item });
+};
+
 export const getItemsOptions = {
   schema: getItemsSchema,
-  handler: async (_, reply) => {
-    const res = await fetch(DMY_API);
-    const items = await res.json();
-
-    reply.send({ items });
-  },
+  handler: getItemsHandler,
 };
 
 export const getItemOptions = {
   schema: getItemSchema,
-  handler: async (req: { params: ItemParams }, reply: any) => {
-    const { id } = req.params as ItemParams; // QUESTION: Why do we need to cast req.params as ItemParams and doesn't get to right type from getItemSchema?
-    const res = await fetch(`${DMY_API}${id}`);
-    const item = await res.json();
-
-    reply.send({ item });
-  },
+  handler: getItemHandler,
 };
 
 const itemsRouter = (
