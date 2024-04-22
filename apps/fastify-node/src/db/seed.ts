6ea28d7a1822f52b84a78ba98ea0e479b0d34174
chielbl <chielbleyenbergh@gmail.com>
2024-04-22 16:52:40 +0200

    feat(drizzle): insert feature

--- apps/fastify-node/src/db/seed.ts
+++ apps/fastify-node/src/db/seed.ts
@@ -1,12 +1,12 @@
 import { products as productsTable } from "./schemas/products";
-import { type ProductDBM } from "./schemas";
+import { usersTable, type ProductDBM } from "./schemas";
 import log from "@/log-manager";
 import { db } from ".";
 
 const main = async () => {
   log.info("Running seed...");
+  /*
   // Seed data here
-
   const res = await fetch(`${process.env.DATABASE_SEED_URL}?limit=10`);
   const { products } = (await res.json()) as { products: ProductDBM[] };
   const mappedProducts = products.map(
@@ -14,17 +14,34 @@ const main = async () => {
     (product) => delete product.id && product
   );
   const itemsExist = Boolean((await db.query.products.findMany()).length);
-
   if (itemsExist) {
     log.info("Deleting existing products...");
     await db.delete(productsTable);
     log.info("Deleting completed!");
   }
-
   log.info("Inserting products ...");
   await db.insert(productsTable).values(mappedProducts);
   log.info("Inserting completed!");
+  */
 
+  // Power of inserting data
+  /*
+  const user = await db
+    .insert(usersTable)
+    .values([
+      { name: "John Doe", age: 1, email: "jd@test.be" },
+      { name: "Jane Doe", age: 2, email: "jd2@test.be" },
+    ]) // Power to insert multiple values
+    .returning({ id: usersTable.id, userName: usersTable.name })
+    .onConflictDoUpdate({
+      target: [usersTable.email],
+      set: {
+        name: `updated`,
+        updatedAt: new Date(),
+      },
+    }); // onConflictDoUpdate works only with Postgres and SQLite (optional feature)
+  console.log("🚀 ~ main ~ user:", user);
+  */
   log.info("Seed completed!");
 };
 
